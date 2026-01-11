5141213146a5ccca4b7ba9ef1bb490ed447ee861
51538 <qq212651538@gmail.com>
2026-01-12 03:22:51 +0800

    Update fanchmwrt.sh

--- fanchmwrt.sh
+++ fanchmwrt.sh
@@ -1,4 +1,8 @@
+git clone https://github.com/fanchmwrt/fanchmwrt.git
 cd fanchmwrt
+./scripts/feeds update -a
+./scripts/feeds install -a
+cd ../deconfig/64.config .config
 make defconfig
 make download -j$(nproc)
 make -j$(nproc) || make -j1 V=s
@@ -7,4 +11,5 @@ TARGET_DIR=bin/targets
 FIRMWARE_DIR=../firmware
 \rm -rf "$FIRMWARE_DIR"
 mkdir -p "$FIRMWARE_DIR"
-find "$TARGET_DIR" -type f \( -name "*.bin" -o -name "*.manifest" -o -name "*.buildinfo" -o -name "*squashfs*" \) -exec cp -f {} "$FIRMWARE_DIR/" \;
+#find "$TARGET_DIR" -type f \( -name "*.bin" -o -name "*.manifest" -o -name "*.buildinfo" -o -name "*squashfs*" \) -exec cp -f {} "$FIRMWARE_DIR/" \;
+find "$TARGET_DIR" -type f \( -name "*.*" -o -name "*.manifest" -o -name "*.buildinfo" -o -name "*squashfs*" \) -exec cp -f {} "$FIRMWARE_DIR/" \;
