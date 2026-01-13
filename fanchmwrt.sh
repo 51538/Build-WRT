31372428d9b4987c2c350009336da3e703c9a3e1
51538 <qq212651538@gmail.com>
2026-01-14 01:44:28 +0800

    Update fanchmwrt.sh

--- fanchmwrt.sh
+++ fanchmwrt.sh
@@ -1,10 +1,18 @@
-git clone https://github.com/hanwckf/immortalwrt-mt798x.git
-cd immortalwrt-mt798x
-sed -i '1i src-git passwall2_luci https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main' feeds.conf.default
-sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
+#git clone https://github.com/hanwckf/immortalwrt-mt798x.git
+#cd immortalwrt-mt798x
+#sed -i '1i src-git passwall2_luci https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main' feeds.conf.default
+#sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
+#./scripts/feeds update -a
+#./scripts/feeds install -a
+#cp ../deconfig/fur-602.config .config
+
+git clone -b openwrt-25.12 --single-branch --filter=blob:none https://github.com/immortalwrt/immortalwrt
+cd immortalwrt
+echo "src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git;main" >> "feeds.conf.default"
 ./scripts/feeds update -a
 ./scripts/feeds install -a
-cp ../deconfig/fur-602.config .config
+cp ../deconfig/imm.config .config
+
 make defconfig
 make download -j$(nproc)
 make -j$(nproc) || make -j1 V=s
