c0b5eae61287ca8c08b9349992cfe9bcccfdd6c8
51538 <qq212651538@gmail.com>
2026-01-14 00:55:57 +0800

    Update fanchmwrt.sh

--- fanchmwrt.sh
+++ fanchmwrt.sh
@@ -1,8 +1,10 @@
-git clone https://github.com/fanchmwrt/fanchmwrt.git
-cd fanchmwrt
+git clone https://github.com/hanwckf/immortalwrt-mt798x.git
+cd immortalwrt-mt798x
+sed -i '1i src-git passwall2_luci https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main' feeds.conf.default
+sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
 ./scripts/feeds update -a
 ./scripts/feeds install -a
-cp ../deconfig/64.config .config
+cp ../deconfig/fur-602.config .config
 make defconfig
 make download -j$(nproc)
 make -j$(nproc) || make -j1 V=s
@@ -11,5 +13,5 @@ TARGET_DIR=bin/targets
 FIRMWARE_DIR=../firmware
 \rm -rf "$FIRMWARE_DIR"
 mkdir -p "$FIRMWARE_DIR"
-#find "$TARGET_DIR" -type f \( -name "*.bin" -o -name "*.manifest" -o -name "*.buildinfo" -o -name "*squashfs*" \) -exec cp -f {} "$FIRMWARE_DIR/" \;
-find "$TARGET_DIR" -type f \( -name "*.*" -o -name "*.manifest" -o -name "*.buildinfo" -o -name "*squashfs*" \) -exec cp -f {} "$FIRMWARE_DIR/" \;
+find "$TARGET_DIR" -type f \( -name "*.bin" -o -name "*.manifest" -o -name "*.buildinfo" -o -name "*squashfs*" \) -exec cp -f {} "$FIRMWARE_DIR/" \;
+#find "$TARGET_DIR" -type f \( -name "*.*" -o -name "*.manifest" -o -name "*.buildinfo" -o -name "*squashfs*" \) -exec cp -f {} "$FIRMWARE_DIR/" \;
