746084ea3d6f9da382b5e573e55aee0c722a1697
51538 <qq212651538@gmail.com>
2026-01-14 11:03:26 +0800

    Update fanchmwrt.sh

--- fanchmwrt.sh
+++ fanchmwrt.sh
@@ -6,13 +6,6 @@
 #./scripts/feeds install -a
 #cp ../deconfig/fur-602.config .config
 
-git clone -b openwrt-25.12 --single-branch --filter=blob:none https://github.com/immortalwrt/immortalwrt
-cd immortalwrt
-echo "src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git;main" >> "feeds.conf.default"
-./scripts/feeds update -a
-./scripts/feeds install -a
-cp ../deconfig/imm.config .config
-
 make defconfig
 make download -j$(nproc)
 make -j$(nproc) || make -j1 V=s
@@ -21,5 +14,5 @@ TARGET_DIR=bin/targets
 FIRMWARE_DIR=../firmware
 \rm -rf "$FIRMWARE_DIR"
 mkdir -p "$FIRMWARE_DIR"
-find "$TARGET_DIR" -type f \( -name "*.bin" -o -name "*.manifest" -o -name "*.buildinfo" -o -name "*squashfs*" \) -exec cp -f {} "$FIRMWARE_DIR/" \;
-#find "$TARGET_DIR" -type f \( -name "*.*" -o -name "*.manifest" -o -name "*.buildinfo" -o -name "*squashfs*" \) -exec cp -f {} "$FIRMWARE_DIR/" \;
+#find "$TARGET_DIR" -type f \( -name "*.bin" -o -name "*.manifest" -o -name "*.buildinfo" -o -name "*squashfs*" \) -exec cp -f {} "$FIRMWARE_DIR/" \;
+find "$TARGET_DIR" -type f \( -name "*.*" -o -name "*.manifest" -o -name "*.buildinfo" -o -name "*squashfs*" \) -exec cp -f {} "$FIRMWARE_DIR/" \;
