765e2ee439b6da51357d7db6f50b2d984732cbb7
51538 <qq212651538@gmail.com>
2026-01-14 18:01:11 +0800

    Update fanchmwrt.sh

--- fanchmwrt.sh
+++ fanchmwrt.sh
@@ -6,14 +6,6 @@ cd immortalwrt-mt798x
 ./scripts/feeds install -a
 cp ../deconfig/fur-602.config .config
 
-rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
-git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
-
-# 移除 openwrt feeds 过时的luci版本
-rm -rf feeds/luci/applications/luci-app-passwall
-git clone https://github.com/Openwrt-Passwall/openwrt-passwall2 package/passwall2
-
-
 make defconfig
 make download -j$(nproc)
 make -j$(nproc) || make -j1 V=s
