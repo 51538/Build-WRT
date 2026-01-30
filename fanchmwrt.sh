84b735e28084fb45f362a3ff2a7aed81a7fe6035
51538 <qq212651538@gmail.com>
2026-01-31 00:37:18 +0800

    Update fanchmwrt.sh

--- fanchmwrt.sh
+++ fanchmwrt.sh
@@ -1,19 +1,15 @@
-#git clone -b openwrt-24.10 --single-branch --filter=blob:none https://github.com/immortalwrt/immortalwrt
-git clone -b openwrt-24.10-6.6 --single-branch --filter=blob:none https://github.com/padavanonly/immortalwrt-mt798x-6.6.git immortalwrt-mt798x-6.6
-cd immortalwrt-mt798x-6.6
+git clone -b openwrt-25.12 --single-branch --filter=blob:none https://github.com/immortalwrt/immortalwrt.git openwrt-25.12
+#git clone -b openwrt-24.10-6.6 --single-branch --filter=blob:none https://github.com/padavanonly/immortalwrt-mt798x-6.6.git immortalwrt-mt798x-6.6
+cd openwrt-25.12
 #echo "src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git;main" >> "feeds.conf.default"
 echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 git clone https://github.com/eamonxg/luci-theme-aurora.git package/luci-theme-aurora
-cp ../deconfig/q30pro.config .config
+cp ../deconfig/both.config .config
 
 make defconfig
 make download -j$(nproc)
-make -j$(nproc) || make -j1 V=s
-
-rm -rf .config
-cp ../deconfig/ax3000t.config .config
 make -j$(nproc) || make -j1 V=s
 
 TARGET_DIR=bin/targets
