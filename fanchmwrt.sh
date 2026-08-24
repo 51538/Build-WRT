87cb7fd4acb1f7e3ddee5787ca3dceb76a50ecec
51538 <qq212651538@gmail.com>
2026-08-24 19:58:20 +0800

    Update fanchmwrt.sh

--- fanchmwrt.sh
+++ fanchmwrt.sh
@@ -20,7 +20,8 @@ echo "src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall
 git clone https://github.com/eamonxg/luci-theme-aurora.git package/luci-theme-aurora
 git clone https://github.com/xylz0928/luci-theme-liquid.git package/luci-theme-liquid
 #cp ../deconfig/j30pro_imm2512.config .config
-cp ../deconfig/chasey-dev.config .config
+#cp ../deconfig/chasey-dev.config .config
+cp ../deconfig/Baili.config .config
 
 make defconfig
 make download -j$(nproc)
