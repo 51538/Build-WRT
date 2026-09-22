2d4c74885081b3729ae8218cf2b6371510973e72
51538 <qq212651538@gmail.com>
2026-09-23 01:11:20 +0800

    Update fanchmwrt.sh

--- fanchmwrt.sh
+++ fanchmwrt.sh
@@ -14,8 +14,8 @@ cd immortalwrt
 
 #echo "src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 #echo "src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main" >> "feeds.conf.default"
-sed -i '1i src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main' feeds.conf.default
-sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
+#sed -i '1i src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main' feeds.conf.default
+#sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
 
 
 ./scripts/feeds update -a
